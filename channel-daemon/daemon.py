239ebd9a8f4b97738c4db48bc7e62891241af13e
Andrei Savu <savu.andrei@gmail.com>
2009-07-04 16:59:02 +0300

    added some extra comments

--- channel-daemon/daemon.py
+++ channel-daemon/daemon.py
@@ -1,17 +1,33 @@
 #! /usr/bin/env python
 """
-Message gateway server for django otp
+Message gateway server for django-otp
 
 Implemented send methods:
-    smtp - to:
+    smtp - to:  email address
+    im   - to:  user@jabber-server
+    sms  - to:  phone number
 
 """
+
+__author__ = 'Andrei Savu <contact@andreisavu.ro>'
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 import settings, generator
 import sys,os,xmpp,time
 
 def send_id(channel, params):
+    """
+    Generate a random ID and send it on the channel 
+
+    Parameters:
+        channel - communication channel
+        params  - channel configs
+
+    Returns:
+        False if it fails
+        Random string sent to user on success
+
+    """
     id = generator.id(channel)
     msg = "OTP: %s" % id
     if not send(channel, msg, params):
@@ -19,6 +35,17 @@ def send_id(channel, params):
     return id
 
 def send(channel, msg, params):
+    """
+    Send a message on the requested channel
+
+    Parameters:
+        channel - communication channel
+        msg     - text message
+        params  - channel configs
+
+    Returns:
+        True or False
+    """
     if channel == 'smtp':
         return send_smtp(msg, params)
     elif channel == 'im':
@@ -28,6 +55,16 @@ def send(channel, msg, params):
     return False
 
 def send_smtp(msg, params):
+    """
+    Send a message using smtp
+
+    Parameters:
+        msg     - text message
+        params  - to: destination address
+    
+    Returns:
+        True
+    """
     import mail
     mail.send(serverURL=settings.SMTP['server'], 
         sender=settings.SMTP['sender'],
@@ -37,6 +74,16 @@ def send_smtp(msg, params):
     return True
 
 def send_im(msg, params):
+    """
+    Send a message using jabber/xmpp
+
+    Parameters:
+        msg     - text message
+        params  - to: some-user@some-jabber-enabled-server
+
+    Returns:
+        True or False
+    """
     jid = xmpp.protocol.JID(settings.XMPP['jid'])
     cl = xmpp.Client(jid.getDomain(),debug=[])
     con = cl.connect()
@@ -51,6 +98,17 @@ def send_im(msg, params):
     return True
 
 def send_sms(msg, params):
+    """
+    Send a message using a phone modem
+
+    Parameters:
+        msg     - text message
+        params  - to: destination phone number
+
+    Returns:
+        True or False
+
+    """
     from subprocess import Popen
     command = ['/usr/bin/gsmsendsms', '-d', settings.SMS['dev'], params['to'], msg]
     p = Popen(command)
@@ -60,6 +118,10 @@ def send_sms(msg, params):
     return True
 
 def get_channels():
+    """
+    Get a list of all available channels with some extra
+    data needed for form construction and display
+    """
     return [{
         'id':'smtp',
         'name':'Email',
@@ -78,6 +140,12 @@ def get_channels():
         }]
 
 def main(argv):
+    """
+    Main function
+
+    Start an xml-rpc server on a given port (by default 8000)
+    and listen for message send requests.
+    """
     port = 8000
     if len(argv) > 1:
         port = int(argv[1])
