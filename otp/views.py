eac2cdf7d98b699040b1570896c9987b476546d2
Ionut G. Stan <ionut.g.stan@gmail.com>
2009-07-04 11:18:32 +0300

    added basic login form to home page

--- otp/views.py
+++ otp/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(request):
-    return HttpResponse("Django OTP")
+    return render_to_response('home.html')
