4496a547c607d2b3eee02f7913560ac729ac5b7a
Andrei Savu <savu.andrei@gmail.com>
2009-07-04 13:28:24 +0300

    first version of a simple client library

---
+++ channel-daemon/client.py
@@ -0,0 +1,18 @@
+"""
+Simple client for message gateway server
+"""
+
+import xmlrpclib as rpc
+
+class MessageGateway:
+
+    def __init__(self, url):
+        self.url = url
+        self.server = rpc.ServerProxy(url)
+
+    def send(self, channel, msg, params):
+        return self.server.send(channel, msg, params)
+
+    def send_id(self, channel, params):
+        return self.server.send_id(channel, params)
+
