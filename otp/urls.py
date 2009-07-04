fabadf0a3793c9bcb0e497377c275504aa5f6821
Ionut G. Stan <ionut.g.stan@gmail.com>
2009-07-04 11:03:46 +0300

    added basic home page view

--- otp/urls.py
+++ otp/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls.defaults import *
 
+from otp.views import home
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^$', home),
     # Example:
     # (r'^otp/', include('otp.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
